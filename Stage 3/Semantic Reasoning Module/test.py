b6cccd90e28fa28357afee688169366080bf6989
Ronnie Smith <ronniesmith@outlook.com>
2019-06-26 21:47:21 +0100

    Cleaned up ontologies

--- Stage 3/Semantic Reasoning Module/test.py
+++ Stage 3/Semantic Reasoning Module/test.py
@@ -1,32 +1,15 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+import rdflib
+from rdflib import Graph
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+g = Graph()
+g.parse('sho.owl')
 
-for result in results["results"]["bindings"]:
-    print('%s: %s' % (result["label"]["xml:lang"], result["label"]["value"]))
-
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.addDefaultGraph("https://ronsm.com/courses/mscr-project/sho.owl#")
-sparql.setQuery("""
-    PREFIX sho: <https://ronsm.com/courses/mscr-project/sho.owl#>
+query = """
+    PREFIX sho: <file://sho.owl#>
     SELECT ?activity
     WHERE { 
         sho:bedroom_location_bed sho:hasPossibleActivity ?activity
     }
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-
-print(results)
-
-for result in results["results"]["bindings"]:
-    print('yes')
-    print('%s: %s' % (result["label"]["xml:lang"], result["label"]["value"]))
-    print(result["label"]["value"])
+"""
+for row in g.query(query):
+    print(row.activity)
